aa617ee98533a68c65a061907f8a3a6e9524ba60
romain.denizot <romain.denizot@mistertemp.com>
2022-10-28 17:07:16 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/coffeeMachine.ts

--- src/coffeeMachine.ts
+++ src/coffeeMachine.ts
@@ -1,5 +1,14 @@
+export enum Drink {
+  COFFEE = "C",
+  CHOCOLATE = "H",
+  TEA = "T",
+}
+
 const Order = {
-  orderCoffee: () => 'C',
+  orderCoffee: () => "C",
+  orderTea: () => "T",
+  orderChocolate: () => "H",
+  orderDrink: (drink: string): string => Drink[drink],
 };
 
 export default Order;
--- test/coffeeMachine.test.ts
+++ test/coffeeMachine.test.ts
@@ -1,7 +1,22 @@
-import Order from '../src/coffeeMachine';
+import Order, { Drink } from "../src/coffeeMachine";
 
-describe('Order', () => {
+describe("Order", () => {
   test('should return "C"', () => {
-    expect(Order.orderCoffee()).toBe('C');
+    expect(Order.orderCoffee()).toBe("C");
   });
+
+  test('should return "T"', () => {
+    expect(Order.orderTea()).toBe("T");
+  });
+
+  test('should return "H"', () => {
+    expect(Order.orderChocolate()).toBe("H");
+  });
+
+  test.each(Object.entries(Drink))(
+    "should return %s",
+    ([key, drink]: Drink) => {
+      expect(Order.orderDrink(key)).toBe(drink);
+    }
+  );
 });
